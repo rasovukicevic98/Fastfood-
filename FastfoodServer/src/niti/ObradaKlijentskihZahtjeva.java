1414809fec0ad82408dc15c391d7cae3febc9b44
Raso Vukicevic <rasovukicevic98@gmail.com>
2024-02-06 15:01:53 +0100

    Implementation of operations(create new,delete, edit) for products.(Server side)

--- FastfoodServer/src/niti/ObradaKlijentskihZahtjeva.java
+++ FastfoodServer/src/niti/ObradaKlijentskihZahtjeva.java
@@ -51,8 +51,37 @@ public class ObradaKlijentskihZahtjeva extends Thread{
                         
                         posaljiOdgovor(so);
                         break;
+                    case Operacija.GET_ALL_FOOD_PRODUCTS:
+                        List<Product> products = Controller.getInstance().getAllFoodProducts();
+                        so = new ServerskiOdgovor();
+                        so.setObject(products);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.GET_ALL_RAW_MATERIALS:
+                        List<RawMaterial> rawMaterials = Controller.getInstance().getAllRawMaterials();
+                        so = new ServerskiOdgovor();
+                        so.setObject(rawMaterials);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.ADD_PRODUCT_AND_INGREDIENTS:
+                        
+                        Product product = (Product) kz.getObjekat();
+                        System.out.println(product);
+                        Long ID=Controller.getInstance().addAllIngredientsAndProduct(product.getIngredients(),product);
+                        product.setId(ID);
+                        so = new ServerskiOdgovor();
+                        so.setUspjesno(true);
+                        so.setObject(ID);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.ADD_ALL_INGREDIENTS:
+                        List<Ingredients> ingredients= (List<Ingredients>) kz.getObjekat();
+                        Controller.getInstance().addAllIngredients(ingredients);
+                        so = new ServerskiOdgovor();
+                        so.setUspjesno(true);
+                        posaljiOdgovor(so);
+                        break;
                     case Operacija.ADD_RAW_MATERIAL:
-                        System.out.println("dodajemo raw material");
                         so = new ServerskiOdgovor();
                         RawMaterial rawMaterial = (RawMaterial) kz.getObjekat();
                         boolean good = Controller.getInstance().addRawMaterial(rawMaterial);
@@ -66,16 +95,9 @@ public class ObradaKlijentskihZahtjeva extends Thread{
                         posaljiOdgovor(so);
                         break;
                     case Operacija.GET_ALL_SUPPLIERS:
-                        System.out.println("Vrcemo sve suppliere");
                         so = new ServerskiOdgovor();
                         List<Supplier> suppliers = Controller.getInstance().getAllSuppliers();
                         so.setObject(suppliers);
-                        posaljiOdgovor(so);
-                        break;
-                    case Operacija.GET_ALL_RAW_MATERIALS:
-                        List<RawMaterial> rawMaterials = Controller.getInstance().getAllRawMaterials();
-                        so = new ServerskiOdgovor();
-                        so.setObject(rawMaterials);
                         posaljiOdgovor(so);
                         break;
                     case Operacija.ADD_ALL_RAW_MATERIALS:
@@ -83,13 +105,6 @@ public class ObradaKlijentskihZahtjeva extends Thread{
                         List<RawMaterial> rawMaterials1 = (List<RawMaterial>) kz.getObjekat();
                         boolean good1 = Controller.getInstance().addAllRawMaterials(rawMaterials1);
                         so.setUspjesno(good1);
-                        posaljiOdgovor(so);
-                        break;
-                    case Operacija.DELETE_RAW_MATERIAL:
-                        so = new ServerskiOdgovor();
-                        rawMaterial = (RawMaterial) kz.getObjekat();
-                        good = Controller.getInstance().deleteRawMaterial(rawMaterial);
-                        so.setUspjesno(good);
                         posaljiOdgovor(so);
                         break;
                     case Operacija.ADD_SUPPLIER:
@@ -100,6 +115,70 @@ public class ObradaKlijentskihZahtjeva extends Thread{
                         }else{
                             so.setUspjesno(false);
                         }
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.SAVE_INVOICE:
+                        so = new ServerskiOdgovor();
+                        Invoice invoice = (Invoice) kz.getObjekat();
+                        Controller.getInstance().addInvoice(invoice);
+                        so.setUspjesno(true);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.ADD_PRODUCT:
+                         product = (Product) kz.getObjekat();
+                        System.out.println(product);
+                        ID=Controller.getInstance().addProduct(product);
+                        product.setId(ID);
+                        so = new ServerskiOdgovor();
+                        so.setUspjesno(true);
+                        so.setObject(ID);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.GET_ALL_BEVERAE_PRODUCTS:
+                        products = Controller.getInstance().getAllBeverageProducts();
+                        so = new ServerskiOdgovor();
+                        so.setObject(products);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.GET_ALL_PRODUCTS:
+                        products = Controller.getInstance().getAllProducts();
+                        so = new ServerskiOdgovor();
+                        so.setObject(products);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.GET_ALL_INGREDIENTS:
+                        ingredients = Controller.getInstance().getAllIngredients();
+                        so = new ServerskiOdgovor();
+                        so.setObject(ingredients);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.UPDATE_PRODUCT_FOOD:
+                        so = new ServerskiOdgovor();
+                        product = (Product) kz.getObjekat();
+                        boolean dobar = Controller.getInstance().updateProductFood(product);
+                        so.setUspjesno(dobar);
+                        so.setPoruka("Successdully changed product");
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.DELETE_BEVERAGE_PRODUCT:
+                        so = new ServerskiOdgovor();
+                        product = (Product) kz.getObjekat();
+                        good = Controller.getInstance().deleteBeverageProduct(product);
+                        so.setUspjesno(good);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.DELETE_FOOD_PRODUCT:
+                        so = new ServerskiOdgovor();
+                        product = (Product) kz.getObjekat();
+                        good = Controller.getInstance().deleteFoodProduct(product);
+                        so.setUspjesno(good);
+                        posaljiOdgovor(so);
+                        break;
+                    case Operacija.DELETE_RAW_MATERIAL:
+                        so = new ServerskiOdgovor();
+                        rawMaterial = (RawMaterial) kz.getObjekat();
+                        good = Controller.getInstance().deleteRawMaterial(rawMaterial);
+                        so.setUspjesno(good);
                         posaljiOdgovor(so);
                         break;
                 }
